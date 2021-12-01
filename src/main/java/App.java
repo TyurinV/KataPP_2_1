a88debae114695a5a6b1ffa2b4382449f2110c0c
Vadim T <denfal@mail.ru>
2021-12-01 21:47:50 +0300

    Ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld - " + (bean == beanTwo));
+
+        //сравниваем 2 хелоуворлда по ссылке
+        Cat beanOneCat = (Cat) applicationContext.getBean("cat");
+        Cat beanTwoCat = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat - " + (beanOneCat == beanTwoCat));
     }
 }
